a70b498fb7ae80aac805265bc228450c85d82b6c
pokassun <pokassun@tutanota.com>
2021-09-14 09:17:46 +0800

    force switch rpc to solana

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -17,14 +17,14 @@ export const IDO_STARTS = moment(process.env.NEXT_PUBLIC_IDO_START)
 
 export const RPC_ENDPOINTS: WalletEndpoint[] = [
   {
-    id: 'solana',
+    id: 'solana_rpc',
     network: 'mainnet-beta' as web3.Cluster,
     rpcURL: 'https://api.mainnet-beta.solana.com',
     rpcName: 'Solana RPC',
     commitment: 'processed' as web3.Commitment,
   },
   {
-    id: 'parrot',
+    id: 'parrot_rpc',
     network: 'mainnet-beta' as web3.Cluster,
     rpcURL: 'https://parrot.rpcpool.com',
     rpcName: 'Parrot RPC',
@@ -74,4 +74,4 @@ export const IDO_ENDPOINTS = [
   },
 ]
 
-export const DEFAULT_RPC = RPC_ENDPOINTS.find((i) => i.id === 'solana')
+export const DEFAULT_RPC = RPC_ENDPOINTS.find((i) => i.id === 'solana_rpc')
